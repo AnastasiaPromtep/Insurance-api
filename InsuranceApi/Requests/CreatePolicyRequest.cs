a51c7b5b203de92006d3bdbd9d41edfa1a668001
Anastasia Prom tep <apromtep@gmail.com>
2026-04-07 15:03:29 +0000

    Fix CreatePolicyRequest validation

--- InsuranceApi/Requests/CreatePolicyRequest.cs
+++ InsuranceApi/Requests/CreatePolicyRequest.cs
@@ -3,16 +3,15 @@ using System.ComponentModel.DataAnnotations;
 namespace InsuranceApi.Requests;
 
 public sealed record CreatePolicyRequest(
-    [property: Required]
-    [property: StringLength(50, MinimumLength = 1)]
+    [param: Required]
+    [param: StringLength(50, MinimumLength = 1)]
     string PolicyNumber,
 
-    [property: Required]
-    [property: StringLength(200, MinimumLength = 1)]
+    [param: Required]
+    [param: StringLength(200, MinimumLength = 1)]
     string SubscriberName,
 
-    [property: Range(typeof(decimal), "0.01", "999999999")]
+    [param: Range(typeof(decimal), "0.01", "999999999")]
     decimal PremiumAmount,
 
-    [property: Required]
     DateTime StartDate);
